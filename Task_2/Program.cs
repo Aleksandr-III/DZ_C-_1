c14d92206c320ab35a0355f58725489915a05040
Александр <popovaleksandr201306@gmail.com>
2022-08-11 04:25:09 +0500

    Вторая задача

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int v = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+
+if (a > max) max = a;
+if (v > max) max = v;
+if (d > max) max = d;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
+
