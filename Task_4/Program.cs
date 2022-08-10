6e634a4628560dc70b3b1639b754dc2d48ea120e
Александр <popovaleksandr201306@gmail.com>
2022-08-11 04:59:53 +0500

    Задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while(count <= a)
+{
+    if (count % 2 == 0) 
+    {
+        Console.Write(count + " ");
+    }
+    count+=1;
+}
