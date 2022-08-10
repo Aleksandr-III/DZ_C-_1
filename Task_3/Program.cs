14795c453af444eca615240b4b880ae58e3775e5
Александр <popovaleksandr201306@gmail.com>
2022-08-11 04:30:54 +0500

    3 задача

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
